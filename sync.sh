9177214679ace314562c48a3f6e2527b526bf8ef
Matt Cavallo <mcavallo.git@gmail.com>
2019-10-22 02:20:03 -0500

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 curl -s -o '/tmp/sync_cloud_infrastructure' 'https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/sbin/sync_cloud_infrastructure';
-chmod 0700 '/tmp/sync_cloud_infrastructure';
-. '/tmp/sync_cloud_infrastructure';
+sudo chmod 0700 '/tmp/sync_cloud_infrastructure';
+sudo '/tmp/sync_cloud_infrastructure';
